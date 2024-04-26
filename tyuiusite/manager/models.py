7b87c3525a4068d3f167183cf25e4116922d8451
ZuborevDmitriy <zyborevdima999@gmail.com>
2024-04-26 14:51:17 +0500

    26.04.2024

--- tyuiusite/manager/models.py
+++ tyuiusite/manager/models.py
@@ -1,3 +1,20 @@
+from django.utils import timezone
 from django.db import models
+from creator.models import Anketa
+from django.contrib.auth import get_user_model
+Users=get_user_model()
 
-# Create your models here.
+class Survey(models.Model):
+    survey_title = models.CharField("Заголовок опроса", max_length=100)
+    test = models.ManyToManyField(Anketa, verbose_name="Тесты")
+    students = models.ManyToManyField(Users, verbose_name="Студенты")
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    def is_active(self):
+        now = timezone.now()
+        return self.start_time <= now <= self.end_time
+    class Meta:
+        verbose_name = "Опрос"
+        verbose_name_plural="Опросы"
+    def __str__(self):
+        return self.survey_title
